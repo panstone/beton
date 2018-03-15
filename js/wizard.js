dc6af2fb829ba7b360a7992472dc51ddea92ba4f
panstone <panaskin.dim@yandex.ru>
2018-03-15 23:55:20 +0500

    try

--- js/wizard.js
+++ js/wizard.js
@@ -54,9 +54,10 @@ $(document).ready(function () {
 $(document).ready(function() {
             $('.minus').click(function () {
                 var $input = $(this).parent().find('input');
+               // $input.val(parseInt($input.val()) - 1);
                 var count = parseInt($input.val()) - 1;
-                count = count < 1 ? 1 : count;
-                $input.val(count);
+                 count = count < 0 ? 0 : count;
+                 $input.val(count);
                 $input.change();
                 return false;
             });
